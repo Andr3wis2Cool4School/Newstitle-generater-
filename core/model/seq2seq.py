265c25c276b0b6d03e2759a0f80c539570efd871
unknown <20448589@life.hkbu.edu.hk>
2021-04-04 02:51:57 +0800

    a

--- core/model/seq2seq.py
+++ core/model/seq2seq.py
@@ -4,8 +4,8 @@ import tensorflow as tf
 from tensorflow import keras 
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Embedding, Dense, Input
-from tensorflow.keras.layers.recurrent import LSTM
-from keras.utils import plot_model
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.utils import plot_model
 
 HIDDEN_UNITS = 100
 DEFAULT_BATCH_SIZE = 64
@@ -32,7 +32,7 @@ class Seq2SeqSummarizer():
             self.version = config['version']
 
         encoder_inputs = Input(shape=(None,), name='encoder_inputs')
-        encode_embedding = Embedding(input_dim=self.num_input_tokens, output_dim=HIDDEN_UNITS,
+        encoder_embedding = Embedding(input_dim=self.num_input_tokens, output_dim=HIDDEN_UNITS,
                                      input_length=self.max_input_seq_length, name='encoder_embedding')
 
         encoder_lstm = LSTM(units=HIDDEN_UNITS, return_state=True, name='encoder_lstm')
@@ -43,8 +43,26 @@ class Seq2SeqSummarizer():
         decoder_lstm = LSTM(units=HIDDEN_UNITS, return_state=True, return_sequences=True, name='decoder_lstm')
         decoder_outputs, decoder_state_h, decoder_state_c = decoder_lstm(decoder_inputs, 
                                                                          initial_state=encoder_states)
+
+        decoder_dense = Dense(units=self.num_target_tokens, activation='softmax', name='decoder_dense')
+        decoder_outputs = decoder_dense(decoder_outputs)
+
         model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
-                                                                        
+        plot_model(model, to_file='model.png', show_shapes=True)
+
+        model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+        
+        self.model = model
+
+        self.encoder_model = Model(encoder_inputs, encoder_states)
+        plot_model(self.encoder_model, to_file='self.encoder_model.png',show_shapes=True)
+
+        decoder_state_inputs = [Input(shape=(HIDDEN_UNITS,)), Input(shape=(HIDDEN_UNITS,))]
+        decoder_outputs, state_h, state_c = decoder_lstm(decoder_inputs, initial_state=decoder_state_inputs)
+        decoder_states = [state_h, state_c]
+        decoder_outputs = decoder_dense(decoder_outputs)
+        self.decoder_model = Model([decoder_inputs] + decoder_state_inputs, [decoder_outputs] + decoder_states)
+        plot_model(self.decoder_model, to_file='self.decoder_model.png',show_shapes=True)     
 
 
 
--- run.py
+++ run.py
@@ -1,9 +1,14 @@
 import numpy as np
 import pandas as pd 
+from sklearn.model_selection import train_test_split
 from core.utils.dataprocer import DataLoader
+from core.model.seq2seq import Seq2SeqSummarizer
+
+LOAD_EXISTING_WEIGHTS = False
 
 
 def main():
+    np.random.seed(42)
     data = DataLoader('./data/fake_or_real_news.csv', 'text', 'title')
     X, Y = data.get_X_and_Y()
     print('DATA LOADING.....')
@@ -12,6 +17,16 @@ def main():
 
     config = data.fit_text()
     # print(config)
+    summarizer = Seq2SeqSummarizer(config)
+
+    if LOAD_EXISTING_WEIGHTS:
+        summarizer.load_weights(weight_file_path=Seq2SeqSummarizer.get_weight_file_path(model_dir_path=model_dir_path))
+
+    Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, Y, test_size=0.2, random_state=42)
+    
+
+
+    
 
     
 
